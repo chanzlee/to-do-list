b4c512b22bec64ed019d4ddf734bfd56633bae74
Brian Nelson and Chan Lee <chan@>
2018-08-20 14:33:08 -0700

    add deleting function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,75 @@
 // business logic
-function Place(name, country, landmark, yearVisited, note) {
-  this.name = name;
-  this.country = country;
-  this.landmark = landmark;
-  this.yearVisited = yearVisited;
-  this.note = note;
+function Task(task, specific1, specific2, specific3, date, completed) {
+  this.task = task;
+  this.specific1 = specific1;
+  this.specific2 = specific2;
+  this.specific3 = specific3;
+  this.date = date;
+  this.completed = completed;
 }
 
-Place.prototype.title = function() {
-  return this.name + ", " + this.country;
+Task.prototype.valid = function() {
+  if (this.completed) {
+    return "Completed";
+  } else {
+    return "Incomplete";
+  }
 }
 
+Task.prototype.specifics = function() {
+    return this.specific1 +", "+ this.specific2+ ", " + this.specific3;
+  }
 
-
+var index = 0;
+function AddCompleteButton() {
+    var r=$('<input/>').attr({
+        type: "button",
+        id: "button",
+        value: 'Complete'
+    });
+    $(".completed").append(r);
+  }
 // user logic
 $(document).ready(function() {
   $("form#add-list").submit(function(event) {
     event.preventDefault();
 
+    var inputTask = $("#task").val();
+    var inputSpecific1 = $("#specific1").val();
+    var inputSpecific2 = $("#specific2").val();
+    var inputSpecific3 = $("#specific3").val();
+    var inputDate = $("#date").val();
+    var newTask = new Task(inputTask, inputSpecific1, inputSpecific2, inputSpecific3, inputDate, 0);
 
-    var inputName = $("#name").val();
-    var inputCountry = $("#country").val();
-    var inputLandmark = $("#landmark").val();
-    var inputYearVisited = $("#yearVisited").val();
-    var inputNote = $("#note").val();
-
-
-
-    var newPlace = new Place(inputName, inputCountry, inputLandmark, inputYearVisited, inputNote);
-
-    $("ul#lists").append("<li><span class='eachPlace'>" + newPlace.title() + "</span></li>");
+    $("ul#lists").append(`<li><span class='eachList' id='${newTask.task}'> ${newTask.task} </span></li>`);
 
 
-    $(".eachPlace").last().click(function(){
-      $("#show-list").hide();
-      $("#show-list").slideDown();
-      $("#show-list h2").text(newPlace.title());
-      $(".landmark").text(newPlace.landmark);
-      $(".yearVisited").text(newPlace.yearVisited);
-      $(".note").text(newPlace.note);
+      // "<li><span class='eachList'" + id="" ">" + newTask.task + "</span></li>");
 
-    });
+    $(".eachList").last().click(function(){
+      // $("ul#lists").last().click(function(){
+        $("#show-list").hide();
+        $("#show-list").slideDown();
+        $("#show-list h2").text(newTask.task);
+        $(".task").text(newTask.task);
+        $(".specifics").text(newTask.specifics());
+        $(".date").text(newTask.date);
+        $(".completed").text(newTask.valid());
+        AddCompleteButton();
+        $("#button").last().click(function() {
+          // var x = document.getElementByTagName("li")
+          // x[].remove();
+          // $("ul#lists").closest('.li').remove();
+          alert(`#${newTask.task}`);
+          $(`#${newTask.task}`).remove();
+        });
 
-    var inputs = ["name", "country", "yearVisited", "note", "landmark"];
 
+      });
+
+    var inputs = ["task", "specific1", "specific2", "specific3", "date", "completed"];
     inputs.forEach(function(input) {
       $('input#' + input).val("");
     });
-
   });
-
 });
